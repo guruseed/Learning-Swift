012679e67f6d979ba281932b3f61642e36381f7b
guruseed <guruseed@daum.net>
2019-10-31 22:37:49 +0900

    variable and constant example

---
+++ 01-VariableAndConstant.playground/Contents.swift
@@ -0,0 +1,29 @@
+/*
+ 01 - Variable and Constant
+ Keyword:
+    - var
+    - let
+ Syntax:
+    - (var|let) Name [: DataType]
+ */
+
+import Cocoa
+
+var str = "Hello, playground"       // String
+
+var integerVariable : Int           // Integer variable
+
+integerVariable = 10                // Assign value of 10
+integerVariable = 0.1               // Error. miss type
+
+
+let integerConstant = 15            // Integer constant
+
+integerConstant = 11                // Error. not allow
+
+
+var a, b, c : Int                   // a, b, c are integer variables
+var d : String, e, f : Double       // d is a string variable, e and f are double variables.
+
+
+
